7251fa3d4b74b8075ec0a9fceefa81a2dbfcfd92
Sorp <danvuk11@gmail.com>
2021-11-29 01:20:54 +0200

    review fixes

--- src/App.js
+++ src/App.js
@@ -1,86 +1,85 @@
-import React, { useState } from 'react';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import emailjs from 'emailjs-com';
-import ReCAPTCHA from 'react-google-recaptcha';
-import {Form,Button,Alert, Fade} from 'react-bootstrap';
+import React, { useState } from 'react'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import emailjs from 'emailjs-com'
+import ReCAPTCHA from 'react-google-recaptcha'
+import { Form, Button, Alert, Fade } from 'react-bootstrap'
+import configData from './config.js'
 
-function App() {
-
-  const useLimitedRequests = function(){
-    const limit = 5;
-    const timePeriod = 240 * 1000;
-    const [ requests, setRequests ] = useState([]);
+function App () {
+  const useLimitedRequests = function () {
+    const limit = 5
+    const timePeriod = 240 * 1000
+    const [requests, setRequests] = useState([])
 
     return [
-        requests,
-        function(){
-            const now = Date.now();
+      requests,
+      function () {
+        const now = Date.now()
 
-            if( requests.length > 0 && (requests[0] < now - timePeriod) ){
-                setRequests( requests.slice(1) );
-            }
-
-            if( requests.length < limit ){
-                setRequests([ ...requests, now ]);
-                return now;
-            }
-
-            return 0;
+        if (requests.length && (requests[0] < now - timePeriod)) {
+          setRequests(requests.slice(1))
         }
-    ];
-  };
 
+        if (requests.length < limit) {
+          setRequests([...requests, now])
+          return now
+        }
 
-  const [response, setResponse] = useState(false);
-  const [failed, setFailed] = useState(false);
-  const [sendingForm, setSendingForm] = useState(false);
-  const [captchaKey, setCaptchaKey] = useState(null);
+        return 0
+      }
+    ]
+  }
 
-  const [name, setName] = useState(null);
-  const [email, setEmail] = useState(null);
-  const [message, setMessage] = useState(null);
+  const [response, setResponse] = useState(false)
+  const [failed, setFailed] = useState(false)
+  const [sendingForm, setSendingForm] = useState(false)
+  const [captchaKey, setCaptchaKey] = useState(null)
 
-  const [,addRequest] = useLimitedRequests();
+  const [name, setName] = useState(null)
+  const [email, setEmail] = useState(null)
+  const [message, setMessage] = useState(null)
 
-  function CaptchaCheck(value) {
-    setCaptchaKey(value);
+  const [, addRequest] = useLimitedRequests()
+
+  function CaptchaCheck (value) {
+    setCaptchaKey(value)
   }
 
   const sendEmail = (e) => {
-    e.preventDefault();
-    setSendingForm(true);
+    e.preventDefault()
+    setSendingForm(true)
 
     const data = {
       name: name,
       email: email,
       message: message,
-      "g-recaptcha-response": captchaKey
-    };
+      'g-recaptcha-response': captchaKey
+    }
 
     try {
-      if( addRequest() > 0 ) {
-        emailjs.send('service_l89he0s', 'template_l8t0oid', data, 'user_yvdLAydZ45L4hplgp5nK4')
-        .then((result) => {
-            console.log(result.text);
-            setSendingForm(false);
-        }, (error) => {
-            console.log(error.text);
-            setSendingForm(false);
-            setFailed(true);
-        });
-      }else {
-        setFailed(true);
+      if (addRequest() > 0) {
+        emailjs.send(configData.SERVICE_ID, configData.TEMPLATE_ID, data, configData.USER_ID)
+          .then((result) => {
+            console.log(result.text)
+            setSendingForm(false)
+          }, (error) => {
+            console.log(error.text)
+            setSendingForm(false)
+            setFailed(true)
+          })
+      } else {
+        setFailed(true)
       }
     } catch (error) {
-      console.log(error.text);
-      setSendingForm(false);
-      setFailed(true);
+      console.log(error.text)
+      setSendingForm(false)
+      setFailed(true)
     }
-    
-    e.target.reset();
-    setResponse(true);
-  };
+
+    e.target.reset()
+    setResponse(true)
+  }
 
   return (
     <div>
@@ -88,20 +87,21 @@ function App() {
       <h1 className="mb-3 text-center mt-5">Welcome back!</h1>
 
       {
-        response ?
-        <Fade in={response}>
+        response
+          ? <Fade in={response}>
         {
-          !failed ? 
-          <Alert className="mb-3 mx-5 bg-success d-flex justify-content-center" variant="sucsses" onClose={() => setResponse(false)} dismissible>
+          !failed
+            ? <Alert className="mb-3 mx-5 bg-success d-flex justify-content-center" variant="sucsses" onClose={() => setResponse(false)} dismissible>
             <Alert.Heading>Email sent! Check your email for a prise.</Alert.Heading>
-          </Alert> :
-          <Alert className="mb-3 mx-5 bg-danger d-flex justify-content-center text-white text-center" variant="danger" onClose={() => setResponse(false)} dismissible>
+          </Alert>
+            : <Alert className="mb-3 mx-5 bg-danger d-flex justify-content-center text-white text-center" variant="danger" onClose={() => setResponse(false)} dismissible>
             <Alert.Heading>Oh snap! You got an error! Try again later.</Alert.Heading>
-          </Alert> 
+          </Alert>
         }
-        </Fade> : null
+        </Fade>
+          : null
       }
-      
+
       <Form onSubmit={sendEmail} variant="dark">
         <Form.Group className="mb-3" controlId="name">
           <Form.Label>Name</Form.Label>
@@ -116,7 +116,7 @@ function App() {
           <Form.Control as="textarea" placeholder="Message" rows={3} onChange={e => setMessage(e.target.value)}/>
         </Form.Group>
         <Button variant="primary" type="submit" disabled={sendingForm || captchaKey == null || email == null || email === ''}>
-          {sendingForm ? "Sending..." : "Send"}
+          {sendingForm ? 'Sending...' : 'Send'}
         </Button>
       </Form>
       <ReCAPTCHA
@@ -126,7 +126,7 @@ function App() {
           onChange={CaptchaCheck}
         />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
